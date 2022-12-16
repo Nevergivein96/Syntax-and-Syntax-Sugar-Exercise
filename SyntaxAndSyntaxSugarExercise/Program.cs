eed6d11ce56282c9d121f3904ec68884b939e62a
Spencer Hansen <spencerhansen@gmail.com>
2022-12-16 12:12:10 -0700

    syntax and sugar completed

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+            var response = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater or equal to 9"; 
         }
     }
 }
